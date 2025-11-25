9955f7f4586355b3da1abd57099eacbb75a445c4
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2025-11-25 11:13:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,5 +21,13 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "client/index.html"),
+      },
+    },
+  },
+  server: {
+    middlewareMode: false,
   },
 });
